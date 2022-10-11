7bf3bd685e222d6e3c4d482ee9e0988f683bd205
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-10-11 22:12:24 +0530

    Add files via upload

---
+++ express.js
@@ -0,0 +1,31 @@
+// const express= require("express")
+// const app = express();
+// app.get('',(req,resp)=>{
+//     resp.send("hello, this is home page")
+// });
+// app.get('/about',(req,resp)=>{
+//     resp.send("hello, this is about page");
+// });
+// app.get('/help',(req,resp)=>{
+//     resp.send("hello, this is help ");
+// });
+// app.get('/contact',(req,resp)=>{
+//     resp.send("hello, this is contact page ");
+// });
+// app.listen(5000);
+
+
+
+// const express=require("express")
+// const app=express();
+// app.get("",(req,resp)=>{
+//     console.log("data entered by browser",req.query.name)
+//     resp.send("hello,this is home page");
+// }).listen(5000);
+
+// const express=require("express")
+// const app=express();
+// app.get("",(req,resp)=>{
+//     console.log("data entered by browser",req.query.name)
+//     resp.send("hello,"+req.query.name);
+// }).listen(5000);
